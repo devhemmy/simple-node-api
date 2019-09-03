41fcb924a33d27c42d1ce9566d8cf83c68057899
devibrahem <dev.ibrahembastawi@gmail.com>
2019-09-03 18:17:42 +0200

    implementing the HTTP verbs

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 // Start Express and Routing
 const app = express();
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
 // Connecting to the Data Base and Structuring the Schema
 
 const db = mongoose.connect("mongodb://localhost/bookAPI");
 const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
 
-// Getting the Data from the Data base with an Example for filtering based on genre
-bookRouter.route("/books").get((req, res) => {
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.json(books);
-    }
-  });
-});
-
-//Getting a single Item of the Data Base
-bookRouter.route("/books/:bookId").get((req, res) => {
-  Book.findById(req.params.bookId, (err, books) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.json(books);
-    }
-  });
-});
+// setting up body parser to get data out of the body and request Object
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api", bookRouter);
 
---
+++ routes/bookRouter.js
@@ -0,0 +1,90 @@
+const express = require("express");
+function routes(Book) {
+  //initilizng the Router instance
+  const bookRouter = express.Router();
+
+  bookRouter
+    .route("/books") // Posting a new book into the Book Objectg
+    .post((req, res) => {
+      const book = new Book(req.body);
+      console.log(book);
+      book.save();
+      return res.status(201).json(book);
+    }) // Getting the Data from the Data base with an Example for filtering based on genre
+    .get((req, res) => {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        } else {
+          return res.json(books);
+        }
+      });
+    });
+
+  /* A Middleware in the middle of the request to make some functions and even changes to the request it self before it gets to the server so we can do whatever and modifiy it as we want*/
+
+  bookRouter.use("/books/:bookId", (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+  bookRouter
+    .route("/books/:bookId") //Getting a single Item of the Data Base
+    .get((req, res) => {
+      res.json(req.book);
+    }) // Updating a specific book as a whole
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    }) // Making changes for just a specific field in the book it self as a whole
+    .patch((req, res) => {
+      const { book } = req;
+
+      if (req.body._id) {
+        delete req.body.body._id;
+      }
+      Object.entries(req.body).forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    }) // Deleting a Book from the Data base
+    .delete((req, res) => {
+      req.book.remove(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
+    });
+
+  return bookRouter;
+}
+
+module.exports = routes;
